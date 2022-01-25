ecc4d0b1b817fd062dc386e84818d507a66e870b
Angelo Fallaria <ba.fallaria@gmail.com>
2022-01-25 20:21:24 +0800

    update stuff

--- sxhkd/new_tmux_terminal.sh
+++ sxhkd/new_tmux_terminal.sh
@@ -1,1 +1,13 @@
-tmux a || tmux
+
+first_tmux_session() {
+    tmux \
+    new-session -d -n "nvim" \
+        ~/.config/sxhkd/nvim-forever.sh ';' \
+    attach ';' \
+    new-window ~/.config/sxhkd/zsh-forever.sh
+}
+
+while true
+do
+    tmux attach || first_tmux_session
+done
---
+++ sxhkd/nvim-forever.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+# This is literally a meme. If you run this script, you can never exit Vim.
+
+while true
+do
+    nvim ~/projects
+done
