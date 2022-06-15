92ef1a01ae995863895380525d14a6faa0e7b9b8
Angelo Fallaria <ba.fallaria@gmail.com>
2022-06-16 07:10:59 +0800

    nvim(lsp): remove commented out code

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -1,6 +1,3 @@
--- vim.cmd [[autocmd! ColorScheme * highlight NormalFloat guibg=#1f2335]]
--- vim.cmd [[autocmd! ColorScheme * highlight FloatBorder guifg=white guibg=#1f2335]]
-
 local border = {
   {"┏", "FloatBorder"},
   {"━", "FloatBorder"},
