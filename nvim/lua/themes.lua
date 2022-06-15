0b7ed71790ad0c722601cd44320026908c77a62a
Angelo Fallaria <ba.fallaria@gmail.com>
2022-06-15 18:01:37 +0800

    nvim: modify hunk unstage bindings

--- nvim/lua/themes.lua
+++ nvim/lua/themes.lua
@@ -48,9 +48,9 @@ require('gitsigns').setup{
 
     -- Actions
     map({'n', 'v'}, '<leader>hs', ':Gitsigns stage_hunk<CR>')
-    map({'n', 'v'}, '<leader>hr', ':Gitsigns reset_hunk<CR>')
+    map({'n', 'v'}, '<leader>hu', ':Gitsigns reset_hunk<CR>')
     map('n', '<leader>hS', gs.stage_buffer)
-    map('n', '<leader>hu', gs.undo_stage_hunk)
+    map('n', '<leader>hr', gs.undo_stage_hunk)
     map('n', '<leader>hR', gs.reset_buffer)
     map('n', '<leader>hp', gs.preview_hunk)
     map('n', '<leader>hb', function() gs.blame_line{full=true} end)
