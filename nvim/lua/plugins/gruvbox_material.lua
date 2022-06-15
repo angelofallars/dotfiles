23c482d6f19ca8f4716f9e1cf067f6bbc72bdabd
Angelo Fallaria <ba.fallaria@gmail.com>
2022-06-16 07:10:01 +0800

    nvim(gruvbox): improve formatting of plugin file

--- nvim/lua/plugins/gruvbox_material.lua
+++ nvim/lua/plugins/gruvbox_material.lua
@@ -1,12 +1,16 @@
-vim.g.gruvbox_material_palette = "material"
-vim.g.gruvbox_material_enable_bold = 1
-vim.g.gruvbox_material_enable_italic = 1
-vim.g.gruvbox_material_statusline_style = "material"
+vim.g.gruvbox_material_palette                   = "material"
+vim.g.gruvbox_material_statusline_style          = "material"
+
+vim.g.gruvbox_material_enable_bold               = 1
+vim.g.gruvbox_material_enable_italic             = 1
+
 vim.g.gruvbox_material_diagnostic_text_highlight = 1
 vim.g.gruvbox_material_diagnostic_line_highlight = 1
-vim.g.gruvbox_material_diagnostic_virtual_text = 'colored'
-vim.g.gruvbox_material_better_performance = 1
-vim.g.gruvbox_material_transparent_background = 1
+vim.g.gruvbox_material_diagnostic_virtual_text   = 'colored'
+
+vim.g.gruvbox_material_better_performance        = 1
+vim.g.gruvbox_material_transparent_background    = 1
+
 vim.cmd([[colorscheme gruvbox-material]])
 
 local function highlight_line_number()
