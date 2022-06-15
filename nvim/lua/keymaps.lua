be7efac9334f56c51e7900494097ece1b206a2c2
Angelo Fallaria <ba.fallaria@gmail.com>
2022-06-15 17:14:28 +0800

    nvim: hide untracked in .config/ for telescope

--- nvim/lua/keymaps.lua
+++ nvim/lua/keymaps.lua
@@ -23,6 +23,10 @@ vim.g.mapleader = " "
 -- Search for <files> in the git repository
 nmap("<C-p>", ":lua require'telescope.builtin'.git_files{show_untracked = true}<cr>")
 
+vim.api.nvim_create_autocmd('BufRead',
+  { pattern = '*',
+    command = 'nnoremap <C-p> :lua require"telescope.builtin".git_files{show_untracked = false}<cr>' })
+
 nmap("<leader>ff", "<cmd>lua require('telescope.builtin').find_files()<cr>")
 nmap("<leader>fg", "<cmd>lua require('telescope.builtin').live_grep(require('telescope.themes').get_ivy({}))<cr>")
 nmap("<leader>fb", "<cmd>lua require('telescope.builtin').buffers()<cr>")
