541af0682be86724e4416a3c2870714ccf020762
Angelo Fallaria <ba.fallaria@gmail.com>
2022-06-15 18:48:17 +0800

    nvim: remove unecessary handlers var

--- nvim/lua/lsp.lua
+++ nvim/lua/lsp.lua
@@ -12,11 +12,6 @@ local border = {
   {"┃", "FloatBorder"},
 }
 
-local handlers =  {
-  ["textDocument/hover"] =  vim.lsp.with(vim.lsp.handlers.hover, {border = border}),
-  ["textDocument/signatureHelp"] =  vim.lsp.with(vim.lsp.handlers.signature_help, {border = border }),
-}
-
 local orig_util_open_floating_preview = vim.lsp.util.open_floating_preview
 function vim.lsp.util.open_floating_preview(contents, syntax, opts, ...)
   opts = opts or {}
