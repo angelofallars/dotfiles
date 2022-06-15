b1f4283f10d5bb006a4419d0036fd83e7b170eac
Angelo Fallaria <ba.fallaria@gmail.com>
2022-06-15 16:40:36 +0800

    nvim: disable changing floating colorscheme in lsp

--- nvim/lua/lsp.lua
+++ nvim/lua/lsp.lua
@@ -1,5 +1,5 @@
-vim.cmd [[autocmd! ColorScheme * highlight NormalFloat guibg=#1f2335]]
-vim.cmd [[autocmd! ColorScheme * highlight FloatBorder guifg=white guibg=#1f2335]]
+-- vim.cmd [[autocmd! ColorScheme * highlight NormalFloat guibg=#1f2335]]
+-- vim.cmd [[autocmd! ColorScheme * highlight FloatBorder guifg=white guibg=#1f2335]]
 
 local border = {
   {"┏", "FloatBorder"},
