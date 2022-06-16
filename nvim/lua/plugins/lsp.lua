70c232541554d9e10042f0132d571fbaebf92a3b
Angelo Fallaria <ba.fallaria@gmail.com>
2022-06-16 18:34:57 +0800

    nvim(lsp): add ltex language server for grammar

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -89,7 +89,7 @@ local lspconfig = require('lspconfig')
 local servers = { 'pylsp', 'pyright', 'bashls', 'clangd',
   'html', 'cssls', 'jsonls', 'tsserver',
   'eslint', 'sqlls', 'vimls', 'rust_analyzer',
-  'dockerls', 'racket_langserver'}
+  'dockerls', 'racket_langserver', 'ltex'}
 
 for _, lsp in pairs(servers) do
   lspconfig[lsp].setup {
