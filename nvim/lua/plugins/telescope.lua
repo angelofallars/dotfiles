a6794b70b4d27ba24953030aae8f4b2d99be5a64
Angelo Fallaria <ba.fallaria@gmail.com>
2022-06-16 09:14:57 +0800

    nvim(telescope): improve layout of telescope

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -15,6 +15,18 @@ require('telescope').setup{
     selection_caret = "  ",
     entry_prefix = "  ",
 
+    sorting_strategy = "ascending",
+    layout_config = {
+      horizontal = {
+        prompt_position = "top",
+        preview_width = 0.55,
+        results_width = 0.45,
+      },
+      width = 0.87,
+      height = 0.80,
+      preview_cutoff = 120,
+    },
+
     borderchars = { "─", "│", "─", "│", "┌", "┐", "┘", "└" },
   },
   pickers = {
