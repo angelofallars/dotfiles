4da948f7af353e67282751e6395f2e923573ce34
Angelo Fallaria <ba.fallaria@gmail.com>
2022-06-18 03:40:46 +0800

    nvim(cmp): remove cmp for : commands

--- nvim/lua/plugins/config/cmp.lua
+++ nvim/lua/plugins/config/cmp.lua
@@ -100,15 +100,6 @@ cmp.setup.cmdline('?', {
   }
 })
 
--- Use cmdline & path source for ':' (if you enabled `native_menu`, this won't work anymore).
-cmp.setup.cmdline(':', {
-  sources = cmp.config.sources({
-    { name = 'path' }
-  }, {
-      { name = 'cmdline' }
-    })
-})
-
 local lspkind = require('lspkind')
 cmp.setup {
   formatting = {
