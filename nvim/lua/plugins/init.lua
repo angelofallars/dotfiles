af8ebb682e0f72e92298eaf87b00d1dcca39464f
Angelo Fallaria <ba.fallaria@gmail.com>
2022-06-17 17:06:41 +0800

    nvim: update telescope-fzf build command

--- nvim/lua/plugins/init.lua
+++ nvim/lua/plugins/init.lua
@@ -70,7 +70,7 @@ local plugins = function(use)
   use {
     'nvim-telescope/telescope-fzf-native.nvim',
     disable = true,
-    run = 'make'
+    run = 'cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release && cmake --install build --prefix build'
   }
 
   use {
