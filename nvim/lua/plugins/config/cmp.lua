55f760b1650f8a507ccfb59d5a0286a947aa2411
Angelo Fallaria <ba.fallaria@gmail.com>
2022-06-18 03:43:18 +0800

    nvim: remove cmp text menu section

--- nvim/lua/plugins/config/cmp.lua
+++ nvim/lua/plugins/config/cmp.lua
@@ -107,15 +107,6 @@ cmp.setup {
       mode = 'symbol_text', -- show only symbol annotations
       maxwidth = 50, -- prevent the popup from showing more than provided characters (e.g 50 will not show more than 50 characters)
 
-      menu = ({
-        buffer = "[Buffer]",
-        nvim_lsp = "[LSP]",
-        vsnip = "[VSnip]",
-        luasnip = "[LuaSnip]",
-        nvim_lua = "[Lua]",
-        latex_symbols = "[Latex]",
-      }),
-
       -- The function below will be called before any actual modifications from lspkind
       -- so that you can provide more controls on popup customization. (See [#30](https://github.com/onsails/lspkind-nvim/pull/30))
       before = function (entry, vim_item)
