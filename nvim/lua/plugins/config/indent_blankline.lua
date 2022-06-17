7924c2f22507cd3756ccdd8c1ad8ec580e8d771a
Angelo Fallaria <ba.fallaria@gmail.com>
2022-06-17 20:14:21 +0800

    nvim(indent_blankline): improve looks

--- nvim/lua/plugins/config/indent_blankline.lua
+++ nvim/lua/plugins/config/indent_blankline.lua
@@ -2,6 +2,18 @@ vim.opt.list = true
 
 require("indent_blankline").setup {
   use_treesitter = true,
-  show_current_context = true,
-  show_current_context_start = true,
+  char = "▏",
+  filetype_exclude = {
+    "help",
+    "terminal",
+    "packer",
+    "lspinfo",
+    "TelescopePrompt",
+    "TelescopeResults",
+    "lsp-installer",
+    "",
+  },
+  buftype_exclude = { "terminal" },
+  show_trailing_blankline_indent = false,
+  show_first_indent_level = false,
 }
